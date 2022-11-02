5b0c3abfe753ce9e92a7103546d6b9891bba4f80
Kaushik <shyamkoushik1211@gmail.com>
2022-11-02 17:43:17 +0530

    modified Postgres

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ Bootstrap(app)
 login_manager = LoginManager(app)
 
 ##CONNECT TO DB
-app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv('DATABASE_URI')
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv('DATABASE_URI','sqlite:///../blog.db')
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 
